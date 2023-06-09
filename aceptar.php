b0493917e1d41bc6e4b38c8b34e6a1224e2c4600
Alejo2003Castiblanco <dagaravito54@misena.edu.co>
2023-06-09 12:59:49 -0500

    wert

--- aceptar.php
+++
@@ -1,9 +0,0 @@
-<?php
-$Nombres = $_get['usuario'];
-$Identificación = $_get['clave'];
-$Destino = $_get['erto'];
-$Correo = $_get['CorreoE'];
-$Genero = $_get['gen'];
-$Fecha = $_get['fecha'];
-$Peso_Equipaje =$_get['peso']
-?>
